d2866b2ff9554b33ed13c9f0221ae8e957fb5fbe
Stella Cannefax <stella@unity3d.com>
2019-01-18 20:04:49 -0800

    add ImageData struct

---
+++ Assets/Runtime/Scripts/Kernels/Data/ImageData.cs
@@ -0,0 +1,20 @@
+using Unity.Collections;
+using UnityEngine;
+
+namespace BurstVision
+{
+    public struct ImageData<T>
+        where T: struct
+    {
+        public int Width;
+        public int Height;
+        public NativeArray<T> Buffer;
+
+        public ImageData(Texture2D texture)
+        {
+            Width = texture.width;
+            Height = texture.height;
+            Buffer = texture.GetRawTextureData<T>();
+        }
+    }
+}
--- Assets/Runtime/Scripts/Operations/Jobs/PoolingJobs.cs
+++ Assets/Runtime/Scripts/Operations/Jobs/PoolingJobs.cs
@@ -7,17 +7,15 @@ namespace BurstVision
     [BurstCompile]
     public struct MaxPool2x2Job : IJob
     {
-        [ReadOnly] public Kernel<short> Kernel;
         [ReadOnly] public NativeArray<byte> Input;
         [WriteOnly] public NativeArray<float> Output;
         
         public int Height;
         public int Width;
 
-        public MaxPool2x2Job(Kernel<short> kernel, NativeArray<byte> input, NativeArray<float> output,
+        public MaxPool2x2Job(NativeArray<byte> input, NativeArray<float> output,
             int width, int height, int xStride = 1, int yStride = 1)
         {
-            Kernel = kernel;
             Input = input;
             Output = output;
             Width = width;
@@ -26,7 +24,7 @@ namespace BurstVision
 
         public void Execute()
         {
-            Kernel.Convolve(Input, Output, Width, Height);
+            
         }
     }
 }
