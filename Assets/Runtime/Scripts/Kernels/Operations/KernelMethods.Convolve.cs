fa6e64f247b471caf470bf36b7a76ff632558a63
Stella Cannefax <stella@unity3d.com>
2019-01-18 20:00:05 -0800

    split jobs out from operation implementations:

--- Assets/Runtime/Scripts/Kernels/Operations/KernelMethods.Convolve.cs
+++ Assets/Runtime/Scripts/Kernels/Operations/KernelMethods.Convolve.cs
@@ -1,90 +1,7 @@
-using Unity.Burst;
 using Unity.Collections;
-using Unity.Jobs;
-using Unity.Mathematics;
 
 namespace BurstVision
 {
-    [BurstCompile]
-    public struct ShortKernelConvolveJob : IJob
-    {
-        [ReadOnly] public Kernel<short> Kernel;
-        [ReadOnly] public NativeArray<byte> Input;
-        [WriteOnly] public NativeArray<float> Output;
-        
-        public int Height;
-        public int Width;
-
-        public ShortKernelConvolveJob(Kernel<short> kernel, NativeArray<byte> input, NativeArray<float> output,
-            int width, int height, int xStride = 1, int yStride = 1)
-        {
-            Kernel = kernel;
-            Input = input;
-            Output = output;
-            Width = width;
-            Height = height;
-        }
-
-        public void Execute()
-        {
-            Kernel.Convolve(Input, Output, Width, Height);
-        }
-    }
-    
-    [BurstCompile]
-    public struct FloatKernelConvolveJob : IJob
-    {
-        [ReadOnly] public Kernel<float> Kernel;
-        [ReadOnly] public NativeArray<byte> Input;
-        [WriteOnly] public NativeArray<float> Output;
-        
-        public int Height;
-        public int Width;
-
-        public FloatKernelConvolveJob(Kernel<float> kernel, NativeArray<byte> input, NativeArray<float> output,
-            int width, int height, int xStride = 1, int yStride = 1)
-        {
-            Kernel = kernel;
-            Input = input;
-            Output = output;
-            Width = width;
-            Height = height;
-        }
-
-        public void Execute()
-        {
-            Kernel.Convolve(Input, Output, Width, Height);
-        }
-    }
-
-    
-    [BurstCompile]
-    public struct MaxPool2x2Job : IJob
-    {
-        [ReadOnly] public Kernel<short> Kernel;
-        [ReadOnly] public NativeArray<byte> Input;
-        [WriteOnly] public NativeArray<float> Output;
-        
-        public int Height;
-        public int Width;
-
-        public MaxPool2x2Job(Kernel<short> kernel, NativeArray<byte> input, NativeArray<float> output,
-            int width, int height, int xStride = 1, int yStride = 1)
-        {
-            Kernel = kernel;
-            Input = input;
-            Output = output;
-            Width = width;
-            Height = height;
-        }
-
-        public void Execute()
-        {
-            Kernel.Convolve(Input, Output, Width, Height);
-        }
-    }
-    
-    
     public static partial class KernelMethods
     {
         public static void Convolve(this Kernel<short> kernel, 
@@ -112,15 +29,15 @@ namespace BurstVision
         public static void Convolve(this Kernel<float> kernel, 
             NativeArray<byte> pixelBuffer, NativeArray<float> pixelOut,
             int width, int height, 
-            uint xStride = 1, uint yStride = 1)
+            int xStride = 1, int yStride = 1)
         {
             var xPad = (kernel.Width - 1) / 2;
             var yPad = (kernel.Height - 1) / 2;
             
-            for (var r = yPad; r < height - yPad; r++)
+            for (var r = yPad; r < height - yPad; r += yStride)
             {
                 var rowIndex = r * width;
-                for (var c = xPad; c < width - xPad; c++)
+                for (var c = xPad; c < width - xPad; c += xStride)
                 {
                     var centerPixelIndex = rowIndex + c;
                     // TODO make padding and kernel offsets different values
---
+++ Assets/Runtime/Scripts/Operations/Jobs/ConvolutionJobs.cs
@@ -0,0 +1,58 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+
+namespace BurstVision
+{
+    [BurstCompile]
+    public struct ShortKernelConvolveJob : IJob
+    {
+        [ReadOnly] public Kernel<short> Kernel;
+        [ReadOnly] public NativeArray<byte> Input;
+        [WriteOnly] public NativeArray<float> Output;
+        
+        public int Height;
+        public int Width;
+
+        public ShortKernelConvolveJob(Kernel<short> kernel, NativeArray<byte> input, NativeArray<float> output,
+            int width, int height, int xStride = 1, int yStride = 1)
+        {
+            Kernel = kernel;
+            Input = input;
+            Output = output;
+            Width = width;
+            Height = height;
+        }
+
+        public void Execute()
+        {
+            Kernel.Convolve(Input, Output, Width, Height);
+        }
+    }
+    
+    [BurstCompile]
+    public struct FloatKernelConvolveJob : IJob
+    {
+        [ReadOnly] public Kernel<float> Kernel;
+        [ReadOnly] public NativeArray<byte> Input;
+        [WriteOnly] public NativeArray<float> Output;
+        
+        public int Height;
+        public int Width;
+
+        public FloatKernelConvolveJob(Kernel<float> kernel, NativeArray<byte> input, NativeArray<float> output,
+            int width, int height, int xStride = 1, int yStride = 1)
+        {
+            Kernel = kernel;
+            Input = input;
+            Output = output;
+            Width = width;
+            Height = height;
+        }
+
+        public void Execute()
+        {
+            Kernel.Convolve(Input, Output, Width, Height);
+        }
+    }
+}
