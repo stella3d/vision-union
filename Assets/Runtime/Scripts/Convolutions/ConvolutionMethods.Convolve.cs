30253d139d7703003cf1e7d8eec8a5be73b9c389
Stella Cannefax <stella@unity3d.com>
2019-01-18 22:34:20 -0800

    add float overload for Convolve

--- Assets/Runtime/Scripts/Convolutions/ConvolutionMethods.Convolve.cs
+++ Assets/Runtime/Scripts/Convolutions/ConvolutionMethods.Convolve.cs
@@ -23,5 +23,28 @@ namespace VisionUnion
                 }
             }
         }
+        
+        public static void Convolve<T>(this Convolution<float> convolution, 
+            ImageData<byte> image, ImageData<float> output)
+            where T : struct
+        {
+            var inputBuffer = image.Buffer;
+            var outputBuffer = output.Buffer;
+            var imageWidth = image.Width;
+            var pad = convolution.Padding;
+            var strides = convolution.Stride;
+            var kernel = convolution.Kernel;
+
+            for (var r = pad.y; r < image.Height - pad.y; r += strides.y)
+            {
+                var rowIndex = r * imageWidth;
+                for (var c = pad.x; c < imageWidth - pad.x; c += strides.x)
+                {
+                    var i = rowIndex + c;
+                    var kernelSum = kernel.Accumulate(inputBuffer, i, imageWidth, pad.x, pad.y);
+                    outputBuffer[i] = kernelSum;
+                }
+            }
+        }
     }
 }
