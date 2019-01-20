ccdcb4b9fba9780e014c709ba009c590cf281bb6
Stella Cannefax <stella@unity3d.com>
2019-01-19 22:06:51 -0800

    add pad test stub:

--- Assets/Tests/Editor/PadTests.cs
+++ Assets/Tests/Editor/PadTests.cs
@@ -1,48 +1,29 @@
-﻿using System.Collections;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Unity.Collections;
 
 namespace VisionUnion.Tests
 {
-	public partial class PadTests
+	public class PadTests
 	{
-		static readonly byte[] k_Bytes6x6 =
-		{
-			3, 4, 2, 1, 5, 3,
-			0, 2, 1, 3, 1, 2,
-			3, 1, 2, 2, 3, 5,
-			2, 3, 0, 2, 2, 6,
-			1, 0, 3, 0, 1, 4,
-			4, 1, 5, 2, 2, 3
-		};
-
-		static readonly byte[] k_Intensities5x5 =
-		{
-			3, 3, 2, 1, 0,
-			0, 0, 1, 3, 1,
-			3, 1, 2, 2, 3,
-			2, 0, 0, 2, 2,
-			2, 0, 0, 0, 1
-		};
-
-
-		NativeArray<float> m_MeanPool;
-		static NativeArray<byte> s_Intensities5x5Native;
+		NativeArray<byte> m_Input5x5;
+		NativeArray<byte> m_Input6x6;
 
 		[OneTimeSetUp]
 		public void BeforeAll()
 		{
-			s_Intensities5x5Native = new NativeArray<byte>(k_Intensities5x5, Allocator.Persistent);
+			m_Input5x5 = new NativeArray<byte>(InputImages.Byte5x5, Allocator.Persistent);
+			m_Input6x6 = new NativeArray<byte>(InputImages.Byte6x6, Allocator.Persistent);
 		}
 
 		[OneTimeTearDown]
 		public void AfterAll()
 		{
-			s_Intensities5x5Native.Dispose();
+			m_Input5x5.Dispose();
+			m_Input6x6.Dispose();
 		}
 
 		[Test]
-		public static void BasicPad()
+		public void BasicPad()
 		{
 			
 		}
