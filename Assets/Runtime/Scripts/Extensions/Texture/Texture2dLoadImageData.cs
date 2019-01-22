a938a1a2f6d80c3f3e8beb55dc93bdb7a95c3e4a
Stella Cannefax <stella@unity3d.com>
2019-01-22 00:38:25 -0800

    add LoadImageData texture extension method:

---
+++ Assets/Runtime/Scripts/Extensions/Texture/Texture2dLoadImageData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace VisionUnion
+{
+    public static partial class Texture2dExtensions
+    {
+        public static void LoadImageData(this Texture2D texture, ImageData<float> data)
+        {
+            texture.LoadRawTextureData(data.Buffer);
+            texture.Apply();
+        }
+    }
+}
