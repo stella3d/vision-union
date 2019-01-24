5efaa33b039266a187f609813b0c2d2195c19fa8
Stella Cannefax <stella@unity3d.com>
2019-01-23 23:11:22 -0800

    add CopySliceToArrayJob

---
+++ Assets/Runtime/Scripts/Processing/CopySliceToArrayJob.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+
+namespace VisionUnion.Jobs
+{
+    public struct CopySliceToArrayJob<T>: IJob
+        where T: struct
+    {
+        [ReadOnly] public NativeSlice<T> Input;
+        [WriteOnly] public NativeArray<T> Output;
+        
+        public CopySliceToArrayJob(NativeSlice<T> input, NativeArray<T> output)
+        {
+            Input = input;
+            Output = output;
+        }
+
+        public void Execute()
+        { 
+            Input.CopyTo(Output);
+        }
+    }
+}
