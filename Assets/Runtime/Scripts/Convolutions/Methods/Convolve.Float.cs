8614333689dde7ef6deab17c4427af985825102a
Stella Cannefax <stella@unity3d.com>
2019-02-23 22:41:55 -0800

    add some float3 overloads:

--- Assets/Runtime/Scripts/Convolutions/Methods/Convolve.Float.cs
+++ Assets/Runtime/Scripts/Convolutions/Methods/Convolve.Float.cs
@@ -1,3 +1,5 @@
+using Unity.Mathematics;
+
 namespace VisionUnion
 {
     public static partial class ConvolutionMethods
@@ -44,6 +46,32 @@ namespace VisionUnion
         
         public static void Convolve(this Convolution2D<float> convolution, 
             Image<float> image, Image<float> output)
+        {
+            var imageWidth = image.Width;
+            var outWidth = output.Width;
+            var outputBuffer = output.Buffer;
+            var stride = convolution.Stride;
+            var kernel = convolution.Kernel2D;
+            var pad = convolution.Padding;
+
+            var outputRow = 0;
+            for (var r = pad.y; r < image.Height - pad.y; r += stride.y)
+            {
+                var rowIndex = r * imageWidth;
+                var outRowIndex = outputRow * outWidth;
+                for (var c = pad.x; c < imageWidth - pad.x; c += stride.x)
+                {
+                    var inIndex = rowIndex + c;
+                    var outIndex = outRowIndex + c - pad.x;
+                    outputBuffer[outIndex] = kernel.Accumulate(image, inIndex);
+                }
+
+                outputRow++;
+            }
+        }
+        
+        public static void Convolve(this Convolution2D<float> convolution, 
+            Image<float3> image, Image<float3> output)
         {
             var imageWidth = image.Width;
             var outWidth = output.Width;
--- Assets/Runtime/Scripts/Kernels/Methods/Accumulate.Float.cs
+++ Assets/Runtime/Scripts/Kernels/Methods/Accumulate.Float.cs
@@ -1,3 +1,5 @@
+using Unity.Mathematics;
+
 namespace VisionUnion
 {
     public static partial class KernelMethods
@@ -79,6 +81,56 @@ namespace VisionUnion
             //convert from 0-255 byte range to 0-1 float range
             const float oneOver255 = 0.0039215686f;
             return sum * oneOver255;
+        }
+        
+        public static float3 Accumulate(this Kernel2D<float> kernel, 
+            Image<float3> image, int centerPixelIndex)
+        {
+            var kernelIndex = 0;
+            var sum = new float3();
+            var pixelBuffer = image.Buffer;
+            var negativeBound = kernel.Bounds.negative;
+            var positiveBound = kernel.Bounds.positive;
+            for (var y = negativeBound.y; y <= positiveBound.y; y++)
+            {
+                var rowOffset = y * image.Width;
+                var rowIndex = centerPixelIndex + rowOffset;
+                for (var x = negativeBound.x; x <= positiveBound.x; x++)
+                {
+                    var pixelIndex = rowIndex + x;
+                    var inputPixelValue = pixelBuffer[pixelIndex];
+                    var kernelMultiplier = kernel.Weights[kernelIndex];
+                    sum += inputPixelValue * kernelMultiplier;
+                    kernelIndex++;
+                }
+            }
+
+            return sum;
+        }
+        
+        public static float3 Accumulate(this Kernel2D<float3> kernel, 
+            Image<float3> image, int centerPixelIndex)
+        {
+            var kernelIndex = 0;
+            var sum = new float3();
+            var pixelBuffer = image.Buffer;
+            var negativeBound = kernel.Bounds.negative;
+            var positiveBound = kernel.Bounds.positive;
+            for (var y = negativeBound.y; y <= positiveBound.y; y++)
+            {
+                var rowOffset = y * image.Width;
+                var rowIndex = centerPixelIndex + rowOffset;
+                for (var x = negativeBound.x; x <= positiveBound.x; x++)
+                {
+                    var pixelIndex = rowIndex + x;
+                    var inputPixelValue = pixelBuffer[pixelIndex];
+                    var kernelMultiplier = kernel.Weights[kernelIndex];
+                    sum += inputPixelValue * kernelMultiplier;
+                    kernelIndex++;
+                }
+            }
+
+            return sum;
         }
     }
 }
