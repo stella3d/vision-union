db8661ae1afb802f23417b79ccbe2a85e67f2010
Stella Cannefax <stella@unity3d.com>
2019-03-23 03:35:31 -0700

    make float convolution jobs disposable

--- Assets/Runtime/Scripts/Convolutions/Jobs/ConvolutionJobs.Float.cs
+++ Assets/Runtime/Scripts/Convolutions/Jobs/ConvolutionJobs.Float.cs
@@ -1,87 +1,14 @@
-﻿using Unity.Burst;
+﻿using System;
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 using Unity.Mathematics;
-using VisionUnion.Organization;
 
 namespace VisionUnion.Jobs
 {
-    /*
-    [BurstCompile]
-    public struct ByteWithFloatConvolveJob : IConvolutionJob<float, byte>
-    {
-        [ReadOnly] public Convolution<float> Convolution;
-        [ReadOnly] public ImageData<byte> Input;
-        [WriteOnly] public ImageData<float> Output;
-
-        public ByteWithFloatConvolveJob(Convolution<float> convolution, 
-            ImageData<byte> input, ImageData<float> output)
-        {
-            Convolution = convolution;
-            Input = input;
-            Output = output;
-        }
-
-        public void SetConvolution(Convolution<float> convolution)
-        {
-            Convolution = convolution;
-        }
-
-        public void SetData(Convolution<float> convolution, ImageData<byte> input, ImageData<float> output)
-        {
-            Convolution = convolution;
-            Input = input;
-            Output = output;
-        }
-
-        public void Execute()
-        {
-            Convolution.Convolve(Input, Output);
-        }
-    }
-    
-    [BurstCompile]
-    public struct ShortWithFloatConvolveJob : IConvolutionJob<float, short>
-    {
-        [ReadOnly] public Convolution<float> Convolution;
-        [ReadOnly] public ImageData<short> Input;
-        [WriteOnly] public ImageData<float> Output;
-
-        public ShortWithFloatConvolveJob(Convolution<float> convolution, 
-            ImageData<short> input, ImageData<float> output)
-        {
-            Convolution = convolution;
-            Input = input;
-            Output = output;
-        }
-        
-        public void SetConvolution(Convolution<float> convolution)
-        {
-            Convolution = convolution;
-        }
-
-        public void SetData(Convolution<float> convolution, ImageData<short> input, ImageData<float> output)
-        {
-            Convolution = convolution;
-            Input = input;
-            Output = output;
-        }
-
-        public void Execute()
-        {
-            Convolution.Convolve(Input, Output);
-        }
-    }
-    */
-
-    public interface IConvolve2D<T1, T2>
-    {
-        
-    }
-
     [BurstCompile]
     // TODO - better names for these jobs!
-    public struct FloatWithFloatConvolveJob : IJob
+    public struct FloatWithFloatConvolveJob : IJob, IDisposable
     {
         [ReadOnly] public Convolution2D<float> Convolution;
         [ReadOnly] public Image<float> Input;
@@ -99,11 +26,17 @@ namespace VisionUnion.Jobs
         {
             Convolution.Convolve(Input, Output);
         }
+
+        public void Dispose()
+        {
+            Convolution.Dispose();
+            Input.Dispose();
+            Output.Dispose();
+        }
     }
     
     [BurstCompile]
-    // TODO - better names for these jobs!
-    public struct FloatWithFloat3ConvolveJob : IJob
+    public struct FloatWithFloat3ConvolveJob : IJob, IDisposable
     {
         [ReadOnly] public Convolution2D<float> Convolution;
         [ReadOnly] public Image<float3> Input;
@@ -120,6 +53,41 @@ namespace VisionUnion.Jobs
         public void Execute()
         {
             Convolution.Convolve(Input, Output);
+        }
+
+        public void Dispose()
+        {
+            Convolution.Dispose();
+            Input.Dispose();
+            Output.Dispose();
+        }
+    }
+    
+    [BurstCompile]
+    public struct Float2WithFloat3ConvolveJob : IJob, IDisposable
+    {
+        [ReadOnly] public Convolution2D<float2> Convolution;
+        [ReadOnly] public Image<float3> Input;
+        [WriteOnly] public Image<float3> Output;
+
+        public Float2WithFloat3ConvolveJob(Convolution2D<float2> convolution, 
+            Image<float3> input, Image<float3> output)
+        {
+            Convolution = convolution;
+            Input = input;
+            Output = output;
+        }
+        
+        public void Execute()
+        {
+            Convolution.Convolve(Input, Output);
+        }
+
+        public void Dispose()
+        {
+            Convolution.Dispose();
+            Input.Dispose();
+            Output.Dispose();
         }
     }
 }
--- Assets/Runtime/Scripts/Convolutions/Methods/Convolve.Float.cs
+++ Assets/Runtime/Scripts/Convolutions/Methods/Convolve.Float.cs
@@ -95,5 +95,31 @@ namespace VisionUnion
                 outputRow++;
             }
         }
+        
+        public static void Convolve(this Convolution2D<float2> convolution, 
+            Image<float3> image, Image<float3> output)
+        {
+            var imageWidth = image.Width;
+            var outWidth = output.Width;
+            var outputBuffer = output.Buffer;
+            var stride = convolution.Stride;
+            var kernel = convolution.Kernel2D;
+            var pad = convolution.Padding;
+
+            var outputRow = 0;
+            for (var r = pad.y; r < image.Height - pad.y; r += stride.y)
+            {
+                var rowIndex = r * imageWidth;
+                var outRowIndex = outputRow * outWidth;
+                for (var c = pad.x; c < imageWidth - pad.x; c += stride.x)
+                {
+                    var inIndex = rowIndex + c;
+                    var outIndex = outRowIndex + c - pad.x;
+                    outputBuffer[outIndex] = kernel.Accumulate(image, inIndex);
+                }
+
+                outputRow++;
+            }
+        }
     }
 }
